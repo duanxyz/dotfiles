912fc7ef940f56a29b830e72cb50ca9b55ad7ba8
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2023-11-15 16:35:21 +0800

    chore: plasma5

--- cells/semar/hosts/semar/default.nix
+++ cells/semar/hosts/semar/default.nix
@@ -9,6 +9,7 @@ in {
     cell.nixosProfiles.core
     cell.nixosProfiles.git
     cell.nixosProfiles.fish
+    cell.nixosProfiles.plasma5
     cell.users.duan
     cell.users.root
   ];
@@ -47,15 +48,6 @@ in {
   hardware.pulseaudio.enable = true;
 
   # programs.fish.enable = true;
-
-  services.openssh.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
 
   nix.settings.trusted-users = [
     "duan"

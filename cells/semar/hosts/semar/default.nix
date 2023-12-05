631fc1c5c91ebe3b863e5a9d5d12fe7fbff5c7ea
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2023-12-05 17:56:27 +0800

    feat: hyprland init

--- cells/semar/hosts/semar/default.nix
+++ cells/semar/hosts/semar/default.nix
@@ -19,7 +19,7 @@ in {
     cell.nixosProfiles.fonts
     cell.nixosProfiles.git
     cell.nixosProfiles.fish
-    cell.nixosProfiles.plasma5
+    cell.nixosProfiles.hyprland
     cell.nixosProfiles.ssh
     cell.nixosProfiles.pipewire
     cell.users.duan
@@ -38,6 +38,7 @@ in {
         cell.homeProfiles.fish
         cell.homeProfiles.alacritty
         cell.homeProfiles.kitty
+        cell.homeProfiles.hyprland
       ];
       home.stateVersion = "23.05";
     };
